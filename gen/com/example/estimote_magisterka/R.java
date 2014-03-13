f1590a5d5c73124148e95ee209b56ef02ba5d923
Beemik <maciej.manczyk@gmail.com>
2014-03-13 23:38:19 +0100

    icon
    new icon added

--- gen/com/example/estimote_magisterka/R.java
+++ gen/com/example/estimote_magisterka/R.java
@@ -22,7 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int beacon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int beaconlocation=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int Row=0x7f08001a;
